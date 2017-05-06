6233b91a0cfb7d8116c5c1ee04224bbdd281468a
sheep0126 <mm53mm17@gmail.com>
2017-05-06 02:41:25 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,8 @@
 class TasksController < ApplicationController
+   before_action :require_user_logged_in
+   
    before_action :set_task, only: [:show, :edit, :update, :destroy]
+   before_action :correct_user, only: [:destroy]
    
   def index
    @tasks = Task.all
@@ -37,9 +40,8 @@ class TasksController < ApplicationController
   end
   def destroy
     @task.destroy
-
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
   private
@@ -50,6 +52,13 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_path
+    end
   end
   
 end
